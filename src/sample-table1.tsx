69926e26e8050f5b8c1b53ba8a6661b2ebca4c8a
abrahambishopcodes <crypticcodetechnologies@gmail.com>
2026-04-27 14:09:55 +0100

    feat: implement TanStack Table with user data and column definitions in SampleTable1

--- src/sample-table1.tsx
+++ src/sample-table1.tsx
@@ -8,7 +8,65 @@ import {
 } from "./components/ui/table";
 import { Card, CardHeader, CardTitle, CardContent } from "./components/ui/card";
 
+import { useReactTable, createColumnHelper, getCoreRowModel } from "@tanstack/react-table";
+
+
 const SampleTable1 = () => {
+
+  // TData type
+    interface User {
+        name: string;
+        email: string;
+        role: "User" | "Merchant" | "Admin";
+        status: "Active" | "Inactive";
+    }
+
+    // Table Data
+    const data: User[] = [
+      {
+        name: "John Larry",
+        email: "larry@gmail.com",
+        role: "Admin",
+        status: "Active"
+      },
+      {
+        name: "Pheobe Jane",
+        email: "phebes123@gmail.com",
+        role: "User",
+        status: "Inactive"
+      },
+      {
+        name: "Sofia Nikky",
+        email: "nikkysoffie@gmail.com",
+        role: "User",
+        status: "Active"
+      },
+      {
+        name: "Bishop Albert",
+        email: "bishopthegreat@gmail.com",
+        role: "Merchant",
+        status: "Active"
+      }
+    ]
+
+    const columnHelper = createColumnHelper<User>();
+
+    // Table columns
+    const columns = [
+      columnHelper.accessor("name", {
+        header: "Name",
+        cell: props => <span>{props.getValue()}</span>
+      })
+    ]
+
+    const table = useReactTable({
+      data: data,
+      columns: columns,
+      getCoreRowModel: getCoreRowModel()
+    })
+
+    const rows = table.getRowModel().rows
+
   return (
     <Card className="p-0">
       <CardHeader className="bg-green-200 p-6">
