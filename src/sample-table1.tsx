e853ba7dd9621a0c728c965837132ff6c2bed527
abrahambishopcodes <crypticcodetechnologies@gmail.com>
2026-04-27 12:01:04 +0100

    feat: wrap SampleTable1 in a Card component with a styled header

--- src/sample-table1.tsx
+++ src/sample-table1.tsx
@@ -1,26 +1,39 @@
-import { Table, TableBody, TableHead, TableHeader, TableRow, TableCell, TableCaption } from "./components/ui/table"
-
+import {
+  Table,
+  TableBody,
+  TableHead,
+  TableHeader,
+  TableRow,
+  TableCell,
+} from "./components/ui/table";
+import { Card, CardHeader, CardTitle, CardContent } from "./components/ui/card";
 
 const SampleTable1 = () => {
   return (
-    <Table>
-        <TableHeader>
+    <Card className="p-0">
+      <CardHeader className="bg-green-200 p-6">
+        <CardTitle>Sample table</CardTitle>
+      </CardHeader>
+      <CardContent className="pb-6">
+        <Table>
+          <TableHeader>
             <TableRow>
-                <TableHead>Name</TableHead>
-                <TableHead>Email</TableHead>
-                <TableHead>Role</TableHead>
+              <TableHead>Name</TableHead>
+              <TableHead>Email</TableHead>
+              <TableHead>Role</TableHead>
             </TableRow>
-        </TableHeader>
-        <TableBody>
-            <TableCaption>Sample table</TableCaption>
+          </TableHeader>
+          <TableBody>
             <TableRow>
-                <TableCell>John Doe</TableCell>
-                <TableCell>[EMAIL_ADDRESS]</TableCell>
-                <TableCell>Admin</TableCell>
+              <TableCell>John Doe</TableCell>
+              <TableCell>[EMAIL_ADDRESS]</TableCell>
+              <TableCell>Admin</TableCell>
             </TableRow>
-        </TableBody>
-    </Table>
-  )
-}
+          </TableBody>
+        </Table>
+      </CardContent>
+    </Card>
+  );
+};
 
-export default SampleTable1
+export default SampleTable1;
