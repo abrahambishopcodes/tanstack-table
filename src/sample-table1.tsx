843c6b4f73997d138fb958dfe028081fce4494ce
abrahambishopcodes <crypticcodetechnologies@gmail.com>
2026-04-28 09:42:32 +0100

    refactor: implement dynamic table rendering using flexRender and header groups

--- src/sample-table1.tsx
+++ src/sample-table1.tsx
@@ -8,76 +8,83 @@ import {
 } from "./components/ui/table";
 import { Card, CardHeader, CardTitle, CardContent } from "./components/ui/card";
 
-import { useReactTable, createColumnHelper, getCoreRowModel } from "@tanstack/react-table";
+import {
+  useReactTable,
+  createColumnHelper,
+  getCoreRowModel,
+  flexRender,
+} from "@tanstack/react-table";
 import { useMemo } from "react";
 
-
 const SampleTable1 = () => {
-
   // TData type
-    interface User {
-        name: string;
-        email: string;
-        role: "User" | "Merchant" | "Admin";
-        status: "Active" | "Inactive";
-    }
+  interface User {
+    name: string;
+    email: string;
+    role: "User" | "Merchant" | "Admin";
+    status: "Active" | "Inactive";
+  }
 
-    // Table Data
-    const data = useMemo<User[]>(
-      () => [
+  // Table Data
+  const data = useMemo<User[]>(
+    () => [
       {
         name: "John Larry",
         email: "larry@gmail.com",
         role: "Admin",
-        status: "Active"
+        status: "Active",
       },
       {
         name: "Pheobe Jane",
         email: "phebes123@gmail.com",
         role: "User",
-        status: "Inactive"
+        status: "Inactive",
       },
       {
         name: "Sofia Nikky",
         email: "nikkysoffie@gmail.com",
         role: "User",
-        status: "Active"
+        status: "Active",
       },
       {
         name: "Bishop Albert",
         email: "bishopthegreat@gmail.com",
         role: "Merchant",
-        status: "Active"
-      }
-    ], [])
+        status: "Active",
+      },
+    ],
+    [],
+  );
 
-    const columnHelper = createColumnHelper<User>();
+  const columnHelper = createColumnHelper<User>();
 
-    // Table columns
-    const columns = useMemo(() => [
+  // Table columns
+  const columns = useMemo(
+    () => [
       columnHelper.accessor("name", {
         header: "Name",
-        cell: props => <span>{props.getValue()}</span>
+        cell: (props) => <span className="font-bold">{props.getValue()}</span>,
       }),
       columnHelper.accessor("email", {
         header: "Email",
-        cell: props => <span>{props.getValue()}</span>
+        cell: (props) => <span>{props.getValue()}</span>,
       }),
       columnHelper.accessor("role", {
         header: "Role",
-        cell: props => <span>{props.getValue()}</span>
-      })
-    ], [columnHelper])
+        cell: (props) => <span>{props.getValue()}</span>,
+      }),
+    ],
+    [columnHelper],
+  );
 
-    // Table instance
-    const table = useReactTable({
-      data: data,
-      columns: columns,
-      getCoreRowModel: getCoreRowModel()
-    })
+  // Table instance
+  const table = useReactTable({
+    data: data,
+    columns: columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
 
-    const rows = table.getRowModel().rows
-
+  const rows = table.getRowModel().rows;
 
   return (
     <Card className="p-0">
@@ -87,22 +94,29 @@ const SampleTable1 = () => {
       <CardContent className="pb-6">
         <Table>
           <TableHeader>
-            <TableRow>
-              <TableHead>Name</TableHead>
-              <TableHead>Email</TableHead>
-              <TableHead>Role</TableHead>
-            </TableRow>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => (
+                  <TableHead colSpan={header.colSpan} key={header.id}>
+                    {flexRender(
+                      header.column.columnDef.header,
+                      header.getContext(),
+                    )}
+                  </TableHead>
+                ))}
+              </TableRow>
+            ))}
           </TableHeader>
           <TableBody>
-            {
-              rows.map(row => (
-                <TableRow key={row.id}>
-                  <TableCell>{row.getValue("name")}</TableCell>
-                  <TableCell>{row.getValue("email")}</TableCell>
-                  <TableCell>{row.getValue("role")}</TableCell>
-                </TableRow>
-              ))
-            }
+            {rows.map((row) => (
+              <TableRow key={row.id}>
+                {row.getVisibleCells().map((cell) => (
+                  <TableCell key={cell.id}>
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))}
           </TableBody>
         </Table>
       </CardContent>
